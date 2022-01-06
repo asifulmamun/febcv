63d729d6bf03d8bafa8454344bc17612a9d6eb83
asifulmamun <asifulmamun@gmail.com>
2022-01-06 21:40:52 +0600

    babel js installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 'use strict';
-
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const babel = require('gulp-babel');
 
+
+// Sass - gulp sass
 function gulpsass() {
-  return gulp.src('./assets/sass/**/*.scss')
+  return gulp.src('./src/sass/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'));
 };
-
-  
-// exports.default = gulpsass;
 exports.sass = gulpsass;
+
+gulp.task('default', () =>
+    gulp.src('./src/js/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('./assets/js'))
+);
